a9c575cf5d7a245ca07977341c35a0e0acd53bab
umutcanbuyuker <umutcandev@gmail.com>
2024-03-11 23:29:08 +0300

    Entity creation

---
+++ VisaApp.Domain/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisaApp.Domain.Common;
+
+namespace VisaApp.Domain.Entities
+{
+    public class Country : EntityBase
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public ICollection<Category> Categories { get; set; }
+
+    }
+}
