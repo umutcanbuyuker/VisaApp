b0ac3e30b6ac04c16b2875ee6125d208077cd272
umut buyuker <can_umut_c@hotmail.com>
2024-03-12 15:42:43 +0300

    Automapper

--- Core/VisaApp.Application/Interface/AutoMapper/IMapper.cs
+++ Core/VisaApp.Application/Interface/AutoMapper/IMapper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,9 @@ namespace VisaApp.Application.Interface.AutoMapper
     public interface IMapper
     {
         TDestination Map<TDestination, TSource>(TSource source, string? ignore = null);
+
+        IList<TDestination> Map<TDestination,TraceSource>(IList<TraceSource> source, string? ignore = null);
+        TDestination Map<TDestination>(object source, string? ignore = null);
+        IList<TDestination> Map<TDestination>(IList<object> source, string? ignore = null);
     }
 }
