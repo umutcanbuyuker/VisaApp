af532c701bcbe92b233624b0b173de39040addb9
umut buyuker <can_umut_c@hotmail.com>
2024-03-15 09:51:31 +0300

    Refresh token generated

---
+++ Core/VisaApp.Application/Features/Auth/RefreshToken/RefreshTokenCommandResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisaApp.Application.Features.Auth.RefreshToken
+{
+    public class RefreshTokenCommandResponse
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
