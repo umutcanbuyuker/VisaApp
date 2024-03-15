63673b65061e166ba629f43e036fefdd81c91566
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-03-15 11:20:58 +0300

    updated and communication started

--- Presentation/ViseApp.Client/src/components/Login/Login.jsx
+++ Presentation/ViseApp.Client/src/components/Login/Login.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Container, TextField } from "@mui/material";
 
 const Login = () => {
-    return (
-        <div>Loginn</div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <Typography
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "100px",
+            fontWeight: "600",
+          }}
+          variant="h4"
+        >
+          Vize Randevu Takibi
+        </Typography>
 
-export default Login
+        <Typography
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "20px",
+            fontSize: "18px",
+            fontWeight: "800",
+            color: "gray",
+          }}
+        >
+          Vize randevu güncellemelerinden haberden olamak için giriş yapın
+        </Typography>
+
+        <TextField
+          label="Kullanıcı Adı"
+          type="text"
+          required
+          maxLength={50}
+          style={{}}
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default Login;
